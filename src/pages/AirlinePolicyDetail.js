cd0874cc184eb21122964d99b9e70d1d9b552fa5
febriansyah <febriansyah.7@gmail.com>
2020-07-21 23:05:31 +0700

    add overlay for search

--- src/pages/AirlinePolicyDetail.js
+++ src/pages/AirlinePolicyDetail.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
-class AirlinePolicyDetail extends Component{
+class AirlinePolicyDetail extends React.Component{
 	constructor(props){
 	   super(props);
 	   this.goBack = this.goBack.bind(this);
@@ -9,17 +9,15 @@ class AirlinePolicyDetail extends Component{
 	goBack(){
 	    this.props.history.goBack();
 	}
+
 	componentDidMount() {
-		 window.readmoreFade();
 		 window.activeAccordion();
-		 window.popupSlider();
 	}
 	render(){
 		return(
 			<div id="middle-content" className="homePage">
 			  <div className="wrapper">
 			    <div className="rows">
-			    	
 			    	<Link to="/" className="back_button"><i className="fa fa-angle-left" aria-hidden="true"></i></Link>
 			    </div>
 			    <div className="rows">
@@ -28,11 +26,11 @@ class AirlinePolicyDetail extends Component{
 					</div>
 				</div>{/* end.rows */}
 
-
-			    <section id="section_innernya">
+				<section id="section_innernya">
 			    	<div className="rows">
 					  <div className="search_row">
-					    <input type="text" id="searchTrigger_airlines" className="search_input trigger_slider_search" data-slider="popup_search_airplane_policy" name="" placeholder="Search Airline" />
+					    <input type="text" id="searchTrigger_airlines" className="search_input" name="" placeholder="Search Airline" />
+					    <div className="overlay_trigger trigger_slider_search" data-slider="popup_search_airplane_policy"></div>
 					  </div>
 					</div>{/* end.rows */}
 			    </section>
