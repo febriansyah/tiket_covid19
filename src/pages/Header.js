42c0fea7e9a9288c8a6a0123d939f66a7eb17ef7
febriansyah <febriansyah.7@gmail.com>
2020-08-25 11:04:52 +0700

    add externlal ink

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -6,28 +6,48 @@ import {
 	Link,
 	useParams
   } from "react-router-dom";
-class Header extends React.Component{
 
+const langnya= window.location.hostname.substr(0, window.location.hostname.indexOf('.'));
+const langDef = 'en'
+
+class Header extends React.Component{
+	constructor(props){
+		super(props)
+		this.state = {
+		  defaultLangnya: langnya == langDef ? langnya : 'id',
+		};
+	}
 	render(){
+		const {
+	      defaultLangnya
+	    } = this.state;
 		return(
 			<div>
 			<header>
 				<div className="wrapperBig">
 					<div className="inner_section">
 						<div className="logo">
-							<Link to="/">
+							<a href="/">
 								<img src="/assets/images/tiket-com-blue@2x.png" alt='logo_tiket' />
-							</Link>
+							</a>
 						</div>{/* end.logo */}
 						<div className="mainMenu">
-							<ul>
-								<li><Link to="/">Flight</Link></li>
-								<li><Link to="/">Hotel</Link></li>
-								<li><Link to="/">Train</Link></li>
-								<li><Link to="/">Car Rental</Link></li>
-								<li><Link to="/">Event</Link></li>
-								<li><Link to="/">Attraction</Link></li>
-								<li><Link to="/">Movie</Link></li>
+							<ul className={`${defaultLangnya == 'id' ? 'hide' : ''}`}>
+								<li><a href="https://en.tiket.com/pesawat">Flight</a></li>
+								<li><a href="https://en.tiket.com/hotel">Hotel</a></li>
+								<li><a href="https://en.tiket.com/to-do">To Dos</a></li>
+								<li><a href="https://en.tiket.com/kereta-api">Train</a></li>
+								<li><a href="https://en.tiket.com/sewa-mobil">Car Rental</a></li>
+								<li><a href="https://en.tiket.com/to-do/search?category=EVENT">Events</a></li>
+							</ul>
+
+							<ul className={`${defaultLangnya == 'id' ? '' : 'hide'}`}>
+								<li><a href="https://www.tiket.com/pesawat">Pesawat</a></li>
+								<li><a href="https://www.tiket.com/hotel">Hotel</a></li>
+								<li><a href="https://www.tiket.com/to-do">To Do</a></li>
+								<li><a href="https://www.tiket.com/kereta-api">Kereta Api</a></li>
+								<li><a href="https://www.tiket.com/sewa-mobil">Sewa Mobil</a></li>
+								<li><a href="https://www.tiket.com/to-do/search?category=EVENT&pageNumber=1">Event</a></li>
 							</ul>
 						</div>
 					</div>
